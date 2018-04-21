317201f5d305c59a5854f1069a68b5c1b200dffb
AnchalNigam <anchalnigamm@gmail.com>
2018-04-20 22:25:22 -0700

    Added all my work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
+  ngOnInit(){
+    this.spinnerService.show();
+    setTimeout(() => {
+      this.spinnerService.hide();
+    }, 2000);
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegionViewComponent } from './region-view/region-view.component';
+import { CountryModule } from './country/country.module';
+import { SharedModule } from './shared/shared.module';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { WorldInfoService } from './world-info.service';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegionViewComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountryModule,
+    SharedModule,
+    HttpClientModule,
+    FormsModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: RegionViewComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: '*',
+        component: RegionViewComponent
+      },
+      {
+        path: '**',
+        component: RegionViewComponent
+      }
+
+
+    ])
   ],
-  providers: [],
+  providers: [WorldInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
